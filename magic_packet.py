b9885e5777aa5d04b1e7d79f83cd8df75577b9cb
mkdubik <mkdubik@gmail.com>
2015-04-05 11:36:26 +0000

    Magic packet sender

---
+++ magic_packet.py
@@ -0,0 +1,17 @@
+import socket
+
+IP, PORT = socket.gethostbyname('zeus'), 7
+
+'''
+The magic packet is a broadcast frame containing anywhere within its payload 6 bytes of all 255 (FF FF FF FF FF FF in hexadecimal), followed by sixteen repetitions of the target computer's 48-bit MAC address, for a total of 102 bytes.
+
+Since the magic packet is only scanned for the string above, and not actually parsed by a full protocol stack, it may be sent as any network- and transport-layer protocol, although it is typically sent as a UDP datagram to port 7 or 9, or directly over Ethernet as EtherType 0x0842
+'''
+padding = ''.join('\xff' for i in xrange(6))
+hwaddr = ''.join('\x1c\x6f\x65\x2c\x9d\x8c' for i in xrange(16))
+
+payload = padding + hwaddr
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(payload, (IP, PORT))
+
