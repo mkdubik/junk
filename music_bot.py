3c967e9988d6e579847ce8648a4acd5dd303066b
mkdubik <mkdubik@gmail.com>
2015-04-06 20:10:47 +0000

    WIP

--- music_bot.py
+++ music_bot.py
@@ -4,8 +4,10 @@
 from pdb import set_trace
 import praw
 import subprocess
+import youtube_dl
 
-class bot:
+
+class MusicBot:
 
 	_subreddits = [
 		'trance',
@@ -14,19 +16,43 @@ class bot:
 		'edm',
 		'electrohouse',
 		'swinghouse',
-		
 	]
 
-	def youtube(self, submission):
+	def youtube_cmd(self, submission):
 		subprocess.call(['youtube-dl', '-t', '--no-mtime', submission.url])
+
+	def my_hook(self, d):
+		if d['status'] == 'finished':
+			print('Done downloading, now converting ...')
+
+	def youtube(self, submissions):
+		ydl_opts = {
+		    'format': 'bestaudio/best',
+		    'postprocessors': [{
+		        'key': 'FFmpegExtractAudio',
+		        'preferredcodec': 'mp3',
+		        'preferredquality': '192',
+		    }],
+		    'logger': MyLogger(),
+		    'progress_hooks': [my_hook],
+		}
+		with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+		    ydl.download(submissions)
 
 	def run(self):
 		r = praw.Reddit(user_agent='music_bot')
-		submissions = r.get_subreddit('trance').get_hot(limit=100)
+		y = []
+		for subreddit in self._subreddits:
+			submissions = r.get_subreddit(subreddit).get_hot(limit=100)
+			for i, x in enumerate(submissions):
+				if 'youtube' in x.url:
+					y.append(x.url)
 
-		for i, x in enumerate(submissions):
-			if 'youtube' in x.url:
-				self.youtube(x)
+		self._remove
+		if y:
+			self.youtube(x)
+		set_trace()
 
-x = bot()
+
+x = MusicBot()
 x.run()
