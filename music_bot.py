add2c5e8a82f905e572ba51ce9519e694ed8bb32
mkdubik <mkdubik@gmail.com>
2015-04-06 21:35:54 +0000

    Ready for work. Much needed to do but I'm lazy

--- music_bot.py
+++ music_bot.py
@@ -7,17 +7,8 @@ import subprocess
 import youtube_dl
 import os
 import sys
-
-
-class MyLogger(object):
-    def debug(self, msg):
-        pass
-
-    def warning(self, msg):
-        pass
-
-    def error(self, msg):
-        print(msg)
+import json
+import sqlite3
 
 
 def my_hook(d):
@@ -28,8 +19,27 @@ class MusicBot:
 
 	def __init__(self, PATH):
 		self.PATH = PATH
+		self.DATABASE = os.getcwd() + '/CACHE.db'
 	
-	def youtube(self, submissions):
+	def get_downloaded_urls(self):
+		try:
+			with sqlite3.connect(self.DATABASE) as connection:
+				cursor = connection.cursor()
+				cursor.execute('SELECT urls FROM music_bot WHERE genre = \'trance\'')
+				return [a[0] for a in cursor.fetchall()]
+		except sqlite3.OperationalError:
+			cursor.execute('CREATE TABLE music_bot(urls TEXT, genre TEXT)')
+			connection.commit()
+			return []
+
+	def write_urls(self, urls, genre = 'trance'):
+		with sqlite3.connect(self.DATABASE) as connection:
+			cursor = connection.cursor()
+			for url in urls:
+				cursor.execute('INSERT INTO music_bot(urls, genre) VALUES (?,?)', (url, genre))
+			connection.commit()
+
+	def youtube(self, urls):
 		ydl_opts = {
 		    'format': 'bestaudio/best',
 		    'postprocessors': [{
@@ -38,25 +48,28 @@ class MusicBot:
 		        'preferredquality': '192',
 		    }],
 			'outtmpl': self.PATH + '%(title)s.%(ext)s',
-		    'logger': MyLogger(),
 		    'progress_hooks': [my_hook],
-			'max_filesize': 400000000,
+		    'max_filesize': 400000000,
 		}
 		with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-		    ydl.download(submissions)
+			ydl.download(urls)
 
 	def run(self):
 		r = praw.Reddit(user_agent='Awesome Music')
 		y = []
 
-		submissions = r.get_subreddit(sys.argv[1]).get_hot(limit=100)
+		downloaded = self.get_downloaded_urls()
+		print downloaded
+		submissions = r.get_subreddit(sys.argv[1]).get_hot(limit=5)
 		for i, x in enumerate(submissions):
+			if x.url in downloaded:
+				continue
 			if 'youtube' in x.url:
 				y.append(x.url)
-
+		
 		if y:
-			self.youtube([x.url])
-		set_trace()
+			self.youtube(y)
+			self.write_urls(y)
 
 if __name__ == '__main__':
 	_subreddits = [
